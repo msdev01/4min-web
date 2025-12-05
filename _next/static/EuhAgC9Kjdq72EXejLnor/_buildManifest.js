8dfc699d45cb98717ccbe83b04fae1950112ef59
Mo-So-coder <so78236873@gmail.com>
2025-12-05 20:32:41 +0900

    Updates

--- _next/static/EuhAgC9Kjdq72EXejLnor/_buildManifest.js
+++ _next/static/EuhAgC9Kjdq72EXejLnor/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/4min-web//_next/:path+",
-        "destination": "/4min-web/_next/:path+"
+        "source": "/4min-web/_next/:path+",
+        "destination": "/_next/:path+"
       }
     ],
     "fallback": []
